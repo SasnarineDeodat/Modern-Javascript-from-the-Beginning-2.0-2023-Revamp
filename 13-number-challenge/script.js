6b5810b25aaaf6d2e3e5b6147e33f45242dd5b91
Robbie213 <sasnarinedeodat407@gmail.com>
2023-08-03 16:41:00 -0400

    Number Challenge

---
+++ 13-number-challenge/script.js
@@ -0,0 +1,24 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+// Get the sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+// Get the difference
+const diff = x - y;
+const diffOutput = `${x}-${y}= ${diff}`;
+
+//Get the product
+const prod = x * y;
+const prodOutput = `${x} * ${y} = ${prod}`;
+
+// Get the quotient
+const quot = x / y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+
+// Get the remainder
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);

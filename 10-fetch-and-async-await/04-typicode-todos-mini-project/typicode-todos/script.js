d0d8ee12b904ef4b373c46d89b23563547907476
Robbie213 <sasnarinedeodat407@gmail.com>
2023-09-04 11:35:26 -0400

    Typicode Todos Mini-Project [2]

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,7 +1,7 @@
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = () => {
-  fetch(apiUrl + "?_limit=5")
+  fetch(apiUrl + "?_limit=10")
     .then((res) => res.json())
     .then((data) => {
       data.forEach((todo) => addTodoToDOM(todo));
@@ -10,6 +10,7 @@ const getTodos = () => {
 
 const addTodoToDOM = (todo) => {
   const div = document.createElement("div");
+  div.classList.add("todo");
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute("data-id", todo.id);
 
@@ -27,20 +28,54 @@ const createTodo = (e) => {
     title: e.target.firstElementChild.value,
     completed: false,
   };
+
   fetch(apiUrl, {
     method: "POST",
     body: JSON.stringify(newTodo),
     headers: {
-      "Content-type": "application/json",
+      "Content-Type": "application/json",
     },
   })
     .then((res) => res.json())
     .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains("todo")) {
+    e.target.classList.toggle("done");
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains("done"));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ completed }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains("todo")) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener("DOMContentLoaded", getTodos);
   document.querySelector("#todo-form").addEventListener("submit", createTodo);
+  document
+    .querySelector("#todo-list")
+    .addEventListener("click", toggleCompleted);
+  document.querySelector("#todo-list").addEventListener("dblclick", deleteTodo);
 };
 
 init();
